9a583799c4d46b9c9f9af159f297fa48ddfc0557
Henrique Mascalhusk <hmascalhusk@id.uff.br>
2017-10-03 15:39:44 -0300

    Coeficientes

--- minquad.c
+++ minquad.c
@@ -24,6 +24,28 @@ void imprime(double *x, double *y)
 	printf("\n");
 }
 
+void coef(double *x, double *y)
+{	int i;
+	double a0, a1, somax, somay, somax2, somaxy;
+	
+	somax=somax2=somay=somaxy=0;
+	printf("Os Resultados são:\n");
+	for(i=0; i<tam; i++)
+	{	
+		somax=somax + x[i];
+		somax2=somax2 + pow(x[i], 2);
+		somay=somay + y[i];
+		somaxy=somaxy + x[i]*y[i];
+		
+	}
+	printf("Soma X = %lf, Soma X² = %lf ,Soma Y = %lf , Soma XY = %lf \n", somax, somax2, somay,somaxy);
+	
+	a0=(somax2*somay - somaxy*somax)/(tam*somax2 -pow(somax, 2));
+	a1=(tam*somaxy - somax*somay)/(tam*somax2 -pow(somax, 2));
+	printf("Os coeficientes são:\n A0 = %lf, A1 = %lf \n", a0, a1);
+	
+}
+
 int main(int argc, char *argv[])
 {	int i;
  	double x[tam], y[tam];
@@ -34,4 +56,6 @@ int main(int argc, char *argv[])
     ler(x, y,leitura);
  	printf ("\nOs dados fornecidos são:\n");
  	imprime(x, y);
+ 	coef(x, y);
+ 	
 }
